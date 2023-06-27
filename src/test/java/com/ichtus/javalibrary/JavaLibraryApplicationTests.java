b3dd04fc5ed881839131b8d9919a18d84b5be6a6
Sergey Lunev <lunev.s@gmail.com>
2023-06-27 20:58:08 +0300

    Initial commit

---
+++ src/test/java/com/ichtus/javalibrary/JavaLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichtus.javalibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
